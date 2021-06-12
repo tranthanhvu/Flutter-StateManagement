f49f33b11a0f1e721aae493b685a2e37400ec9fe
Trần Thanh Vũ <ttvu90@gmail.com>
2021-06-12 23:15:24 +0700

    Create Constant.dart

---
+++ flutter_none/lib/Common/Constant.dart
@@ -0,0 +1,6 @@
+class GlobalConstant {
+  final apiKey = '198ae0eeb631420ba7c159c2e63fdcbe';
+  final apiHost = 'https://api.themoviedb.org/3/movie/550';
+  final apiToken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOThhZTBlZWI2MzE0MjBiYTdjMTU5YzJlNjNmZGNiZSIsInN1YiI6IjYwYzRkOTU1ZWVhMzRkMDA1OGU3ZGUzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B6m3oFFAKsA1thHoYn3QahH7tgSKYS1KJKctfxVvrfA';
+}
